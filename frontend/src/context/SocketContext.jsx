74a72b0246202d198139f9703d015e481f2ddd8a
Shubhendu-Maharana <maharanasubhendu3@gmail.com>
2024-09-20 13:58:31 +0530

    update socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app-gcuy.onrender.com/", {
+      const socket = io("https://chat-web-app-gx5n.onrender.com/", {
         query: {
           userId: authUser._id,
         },
